cbb4cc2299d9d90eaf2058730faa2bdc2a6048f2
ASTER18K <donhon05@gmail.com>
2022-10-27 16:18:16 +0900

    221027m

--- YJWDJ2B-22-2-Kotlin/Android_Project/register/app/src/main/java/com/example/register/MainActivity.kt
+++ YJWDJ2B-22-2-Kotlin/Android_Project/register/app/src/main/java/com/example/register/MainActivity.kt
@@ -2,17 +2,51 @@ package com.example.register
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
+import android.widget.EditText
 import com.example.register.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), TextWatcher() {
-    private val binding: ActivityMainBinding by lazy {
+class MainActivity : AppCompatActivity(){
+    private val binding:ActivityMainBinding by lazy{
         ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private fun updateWidgets(){
+        var progress=0
+        if(binding.editTextTextPersonName.text.isNotEmpty())
+            progress++
+        if(binding.editTextPhone.text.isNotEmpty())
+            progress++
+        // check radio - 선택된 버튼이 없을 때 -1
+        if(binding.radioGroup.checkedRadioButtonId > -1)
+            progress++
+        if(binding.checkBoxEURA.isChecked)
+            progress++
+
+        binding.progressBar.progress = progress
+        binding.buttonApply.isEnabled = progress==binding.progressBar.max
+    }
+
+    private val textWatcher:TextWatcher = object:TextWatcher{
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        override fun afterTextChanged(s: Editable?) {
+            updateWidgets()
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        binding.editTextTextPersonName.addTextChangedListener(this)
+
+        binding.progressBar.max=4
+
+        binding.editTextTextPersonName.addTextChangedListener(textWatcher)
+        binding.editTextPhone.addTextChangedListener(textWatcher)
+
+        binding.radioGroup.setOnCheckedChangeListener { _, _ ->  updateWidgets()}
+        binding.checkBoxEURA.setOnClickListener { updateWidgets() }
     }
 }
